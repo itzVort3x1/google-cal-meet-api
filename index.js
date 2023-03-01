72c9de8cf018d1d914f9a97659e01783188fc40f
itzVort3x1 <saikaustubh10@gmail.com>
2023-03-01 12:22:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+async function meet(options) {
+	const { google } = require("googleapis");
+	const { OAuth2 } = google.auth;
+
+	var date1 =
+		options.date +
+		"T" +
+		options.time.split(":")[0] +
+		options.time.split(":")[1] +
+		":30";
+	var date2 =
+		options.date +
+		"T" +
+		options.time.split(":")[0] +
+		options.time.split(":")[1] +
+		":30";
+
+	var x = new Date(
+		options.date +
+			"T" +
+			options.time.split(":")[0] +
+			options.time.split(":")[1] +
+			":30"
+	);
+	var y = new Date(
+		options.date + "T" + options.time.split(":")[0] + ":45" + ":30"
+	);
+
+	var end1 =
+		options.date +
+		"T" +
+		x.getUTCHours() +
+		":" +
+		x.getUTCMinutes() +
+		":00" +
+		".000Z";
+	var end2 =
+		options.date +
+		"T" +
+		y.getUTCHours() +
+		":" +
+		y.getUTCMinutes() +
+		":00" +
+		".000Z";
+
+	let oAuth2Client = new OAuth2(options.clientId, options.clientSecret);
+
+	oAuth2Client.setCredentials({
+		refresh_token: options.refreshToken,
+	});
+
+	let calendar = google.calendar({ version: "v3", auth: oAuth2Client });
+
+	let result = await calendar.events.list({
+		calendarId: "primary",
+		timeMin: end1,
+		timeMax: end2,
+		maxResults: 1,
+		singleEvents: true,
+		orderBy: "startTime",
+	});
+
+	let events = result.data.items;
+	if (events.length) {
+		throw Error("The Slot Is Not Available");
+	}
+
+	const eventStartTime = new Date();
+	eventStartTime.setDate(options.date.split("-")[2]);
+	const eventEndTime = new Date();
+	eventEndTime.setDate(options.date.split("-")[2]);
+	eventEndTime.setMinutes(eventStartTime.getMinutes() + 45);
+
+	const event = {
+		summary: options.summary,
+		location: options.location,
+		description: options.description,
+		colorId: 1,
+		conferenceData: {
+			createRequest: {
+				requestId: "zzz",
+				conferenceSolutionKey: {
+					type: "hangoutsMeet",
+				},
+			},
+		},
+		start: {
+			dateTime: date1,
+			timeZone: "Asia/Kolkata",
+		},
+		end: {
+			dateTime: date2,
+			timeZone: "Asia/Kolkata",
+		},
+		attendees: options.attendees,
+		reminders: {
+			useDefault: false,
+			overrides: [
+				{ method: "email", minutes: 60 },
+				{ method: "popup", minutes: options.alert },
+			],
+		},
+	};
+
+	let link = await calendar.events.insert({
+		calendarId: "primary",
+		conferenceDataVersion: "1",
+		resource: event,
+	});
+	return link.data.hangoutLink;
+}
+
+module.exports.meet = meet;
